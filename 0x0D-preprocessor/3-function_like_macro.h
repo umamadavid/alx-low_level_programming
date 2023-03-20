94387fd096323ca90defce98b50b108e981fb69e
DAVID UMAMA <107047952+umamadavid@users.noreply.github.com>
2023-03-20 22:34:03 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,15 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 
-/*
- * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
+#endif /* _FUNCTION_LIKE_MACRO_H */
